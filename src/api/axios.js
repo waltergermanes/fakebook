82d97d54a9165598327d823efb1f54c0e5ef29f2
Walter Germanes <78303625+waltergermanes@users.noreply.github.com>
2024-03-30 20:18:18 +0800

    gitignore modified

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,10 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:5000/"
+export const BASE_URL = "https://fakebook.onrender.com"
 
 export default axios.create({
     baseURL: BASE_URL
 })
 
-/* export const loginUser = async(user)=>{
-    return await api.post("/auth", user)
-}  */
-
-/* export const refreshLogin = async()=>{
-    return await api.get("/auth/refresh", {
-        withCredentials: true
-    })
-}  */
-/* export default axios.create({
-    baseURL: BASE_URL
-}); */
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json' },
